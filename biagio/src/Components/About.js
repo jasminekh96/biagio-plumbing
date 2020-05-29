6872730b7bdc7cbd05bb85904df40715f3defce3
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-05-28 22:58:34 -0400

    typo

--- biagio/src/Components/About.js
+++ biagio/src/Components/About.js
@@ -4,7 +4,7 @@ const About = () => {
 	return (
 		<div className='aboutCard'>
 			<p className='aboutPrgh'>
-				<span className='title-about'>Biagio Plumbing and Heating </span> is a small family owned business that my wife
+				<span className='title-about'> Biagio Plumbing and Heating </span> is a small family owned business that my wife
 				and I run. Biagio has been around since 1986. We are based in Central Islip and travel through Suffolk and
 				Nassau County. We take all charge cards and give senior citizen discount. We also take emergency calls all hours
 				of the day, Saturday and Sunday are for emergency calls only. Price varies depending on job. Small jobs, big
