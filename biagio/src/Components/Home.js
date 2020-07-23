76c51d6f40cf8ea67f390dbad4c681993d7cff59
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-07-22 20:59:19 -0400

    cleaned up code

--- biagio/src/Components/Home.js
+++ biagio/src/Components/Home.js
@@ -1,14 +1,7 @@
 import React from 'react';
-// import img1 from '../Images/img1.png';
 import img2 from '../Images/img2.png';
 import img3 from '../Images/img3.png';
-// import img4 from '../Images/img4.png';
-// import img5 from '../Images/img5.png';
-// import img8 from '../Images/img8.png';
-// import img6 from '../Images/img6.png';
 import img7 from '../Images/img7.png';
-
-// import Slider from 'react-slick';
 
 const Home = () => {
 	return (
